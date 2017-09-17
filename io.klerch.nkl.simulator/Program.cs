d301b0be3912ac03db201c07b4a46eb724e92527
kaylerch <dekstar@gmx.de>
2017-09-17 17:21:08 +0200

    Initial commit

---
+++ io.klerch.nkl.simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace io.klerch.nkl.simulator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
